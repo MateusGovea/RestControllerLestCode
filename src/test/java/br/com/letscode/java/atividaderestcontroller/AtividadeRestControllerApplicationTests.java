7c33e7429da9d43e5bc91ae2349f64f86ca178e4
Mateus Rodrigues de Govêa <matrgovea@gmail.com>
2021-10-26 14:06:34 -0300

    Versao atual

---
+++ src/test/java/br/com/letscode/java/atividaderestcontroller/AtividadeRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.java.atividaderestcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeRestControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
